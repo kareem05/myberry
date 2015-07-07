48789d1c0a7085d2bb475053ea356938028a84b9
kareem05 <kareemsaleh32@gmail.com>
2015-07-07 18:36:00 +0200

    heruko update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+
+ def index
+@content_first = 'my conent is heeeeeeeeere'
+ end
+
+ def edit
+
+ end
+
+ def create
+
+ end
+
+ def new
+
+ end
+
+ def update
+
+ end
+
+ def show
+
+ end
+
+ def destroy
+
+ end
+
+end
---
+++ app/helpers/request_example_controller_helper.rb
@@ -0,0 +1,2 @@
+module RequestExampleControllerHelper
+end
